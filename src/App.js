1d9b08302ee82f077ad6028a36d8cc5b7c7b224f
toandv <toandv@vnext.vn>
2022-10-05 17:47:26 +0700

    feat: Love

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img style={{borderRadius: '50%'}} src="https://firebasestorage.googleapis.com/v0/b/thanh-8f7b4.appspot.com/o/processed-8aec9ecf-3286-4f9c-9f9d-5fbefe38468f_p9Ndh4dT.jpeg?alt=media&token=86a1f267-b22e-4dfb-8b84-66bb595572d5" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
